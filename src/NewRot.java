47a990a3e013204647637e7bafd25e7b43ccf1ee
SWSpratlin <120224974+SWSpratlin@users.noreply.github.com>
2024-03-28 12:19:11 -0400

    Updating Javadoc comments.
    Mostly for clarity and planning.

--- src/NewRot.java
+++ src/NewRot.java
@@ -7,22 +7,40 @@ import java.util.Scanner;
 
 public class NewRot {
 
-    // PApplet for "width" and "height" references (could change to
-    // a class variable but for now this works.
+    /**
+     * PApplet for "width" and "height" references (could change to
+     * a class variable but for now this works.
+     */
     PApplet sketch;
+    /**
+     * Class access for the PImage. Necessary for access of the width/height fields.
+     */
+    PImage img;
 
     // Counter for debugging.
     int counter;
     //Max variable for array length purposes
     final int max;
 
-    // pxP is the array to hold the percentage values for each pixel
-    // updater is to mark which pixels need updating.
+    /**
+     * pxP is the array to hold the percentage values for each pixel. Updated using
+     * a combination of the csvSearch() and scan() methods
+     */
     int[] pxP;
+    /**
+     * Updater is the array that marks each eligible pixel for a step in Opacity.
+     * Updated in the colorStep() method, executed and reset in the grow() method.
+     */
     boolean[] updater;
 
+    /**
+     * Constructor.
+     * @param s Usually "This" unless two different sketches in the same doc.
+     * @param w PImage to be referenced. Necessary for width/height access used
+     */
     public NewRot(PApplet s, PImage w){
         this.sketch = s;
+        this. img = w;
 
         // Initialize arrays, and fill them with their default values
         pxP = new int[w.pixels.length];
@@ -37,7 +55,23 @@ public class NewRot {
         File table = new File(tableName);
     }
 
-    private void scan(PImage img){
+    /**
+     *  Used internally as the search method for the CSV.
+     */
+    private int csvSearch(int hex){
+       // P stands for Percent. will be assigned a value from the CSV
+        int p = 0;
+       return p;
+    }
+
+    /**
+     * Used internally INSIDE a for loop to write values to each index of the pxP array.
+     * This method will contain all actions for the scanning and updating of the pxP array
+     * within a single loop of a PImage 
+     */
+    private void scan(){
 
     }
+
+
 }
